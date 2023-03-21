00808a956d73411527cb2066459db821bed87e79
FlorianGrohmann <florian-grohmann@gmx.de>
2023-03-21 16:39:13 +0100

    init project with sample three.js app and a node.js server setup to serve the three.js libary

---
+++ server/server.js
@@ -0,0 +1,48 @@
+//app.use(bodyParser.urlencoded({extended : true}));
+//app.use('/changeBackgroundColor', bodyParser.text());
+const express = require("express");
+const fs = require('fs');
+const path = require('path');
+const bodyParser = require('body-parser');
+const app = express()
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Website files
+
+app.get('/network_viewer', async function(req, res) {
+    res.sendFile(path.resolve(__dirname + "/.." + "/src/network_viewer.html"));
+});
+app.get('/network_viewer.css', async function(req, res) {
+    res.sendFile(__dirname + "/" + "src/network_viewer.css");
+});
+
+// Web content
+
+app.get('/favicon.png', async function(req, res) {
+    res.sendFile(__dirname + "/" + "favicon.png");
+});
+app.get('/jquery-3.6.0.min.js', async function(req, res) {
+    res.sendFile(__dirname + "/" + "jquery-3.6.0.min.js");
+});
+app.get('/raleway-v22-latin-200.woff2', async function(req, res) {
+    res.sendFile(__dirname + "/" + "raleway-v22-latin-200.woff2");
+});
+app.get('/raleway-v22-latin-200.woff', async function(req, res) {
+    res.sendFile(__dirname + "/" + "raleway-v22-latin-200.woff");
+});
+
+// Three.js files
+
+app.use('/build/', express.static(path.join(__dirname, '/res/build')));
+app.use('/jsm/', express.static(path.join(__dirname, 'res/jsm')));
+
+
+// Server functions
+
+app.route('/test').get((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write(lastChange);
+    res.end();
+});
+
+app.listen(9999, "127.0.0.1")
